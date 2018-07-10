ff3fec6427587802ecb5cc605bb887f4030b4e2d
Craig Loomis <cloomis@astro.princeton.edu>
2018-07-10 13:31:02 -0400

    Add "loop loopTime=SS" and "loop off"

--- python/roughActor/Commands/RoughCmd.py
+++ python/roughActor/Commands/RoughCmd.py
@@ -2,6 +2,8 @@ import opscore.protocols.keys as keys
 import opscore.protocols.types as types
 from opscore.utility.qstr import qstr
 
+from twisted.internet import reactor
+
 class RoughCmd(object):
 
     def __init__(self, actor):
@@ -22,6 +24,9 @@ class RoughCmd(object):
             ('pump', 'standby <percent>', self.standby),
             ('pump', 'standby off', self.standbyOff),
 
+            ('loop', '@(off)', self.loop),
+            ('loop', '<loopTime>', self.loop),
+            
             ('gauge', '@raw', self.gaugeRaw),
             ('gauge', 'status', self.pressure),
             ('gauge', '<setRaw>', self.setRaw),
@@ -34,11 +39,16 @@ class RoughCmd(object):
                                                  help='the speed for standby mode'),
                                         keys.Key("getRaw", types.Int(),
                                                  help='the MPT200 query'),
+                                        keys.Key("loopTime", types.Float(),
+                                                 help='how quickly to spin-loop status'),
                                         keys.Key("setRaw",
                                                  types.CompoundValueType(types.Int(help='the MPT200 code'),
                                                                          types.String(help='the MPT200 value'))),
                                         )
 
+        self.loopState = None
+        self.loopId = None
+        
     def roughRaw(self, cmd):
         """ Send a raw command to the rough controller. """
 
@@ -48,6 +58,87 @@ class RoughCmd(object):
         ret = self.actor.controllers[ctrlr].pumpCmd(cmd_txt, cmd=cmd)
         cmd.finish('text="returned %r"' % (ret))
 
+    def _doLoop(self):
+        if self.loopState is None:
+            return
+
+        cmd, loopTime = self.loopState
+        try:
+            hz, errorMask, errorString = self.actor.controllers['pump'].quickStatus(cmd)
+        except Exception as e:
+            hz = 0
+            errorMask = 0xffff
+            errorString = f"FAILED to get pump status: {e}"
+            cmd.warn(f'text="{errorString}"')
+            self._stopLoop(doFail=True)
+            return
+        
+        try:
+            pressure = self.actor.controllers['gauge'].pressure(cmd)
+        except Exception as e:
+            pressure = 9999
+            cmd.warn(f'text="FAILED to get gauge status: {e}"')
+            self._stopLoop(doFail=True)
+            return
+
+        isOK = (# hz == 150 and
+                errorString == "OK" and
+                pressure <= 1000)
+
+        content = ('pressure=%g; pumpSpeed=%d; pumpErrors=0x%04x,%r' %
+                   (pressure, hz, errorMask, errorString))
+        if isOK:
+            cmd.inform(content)
+            self.loopId = reactor.callLater(loopTime, self._doLoop)
+        else:
+            cmd.warn(content)
+            self._stopLoop(cmd, doFail=True)
+            return
+        
+    def _stopLoop(self, cmd=None, doFail=False):
+        if self.loopId is not None:
+            self.loopId.cancel()
+            self.loopId = None
+        
+        if self.loopState is not None:
+            loopCmd, _ = self.loopState
+            loopCmd.finish('text="loop stopped"', fail=doFail)
+            if cmd is not None:
+                cmd.inform('text="loop stopped"')
+        else:
+            if cmd is not None:
+                cmd.warn('text="no loop to stop"')
+        self.loopState = None
+
+    def _startLoop(self, cmd, loopTime):
+        loopTime = max(loopTime, 0.1)
+        savedState = self.loopState
+        if savedState is not None:
+            self.loopState = None
+            raise RuntimeError(f"roughing loop was found running! {savedState}")
+        
+        cmd.inform('text="starting %gs roughing loop' % (loopTime))
+        self.loopState = cmd, loopTime
+        self._doLoop()
+        
+    def loop(self, cmd):
+        """ Start or stop a safety loop """
+        cmdKeys = cmd.cmd.keywords
+        turnOff = 'off' in cmdKeys
+        if turnOff:
+            loopTime = 0
+        else:
+            loopTime = cmdKeys['loopTime'].values[0]
+
+        if loopTime == 0:
+            self._stopLoop(cmd)
+            cmd.finish()
+        else:
+            if self.loopState is not None:
+                self._stopLoop(cmd)
+
+            self._startLoop(cmd, loopTime)
+            
     def ident(self, cmd):
         """ Return the rough ids. 
 
